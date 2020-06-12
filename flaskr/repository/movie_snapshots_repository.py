01f304250b21034a618597e7dedd9912fa3e1e8c
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-12 21:35:34 +0530

    Abhijeet/Sarthak : Implements get_all in MovieSnapshotsRepository

--- flaskr/repository/movie_snapshots_repository.py
+++ flaskr/repository/movie_snapshots_repository.py
@@ -1,3 +1,8 @@
+from typing import List
+
+from flaskr.entity.movie_snapshot import MovieSnapshot
+
+
 class MovieSnapshotsRepository:
-    def get_all(self):
-        pass
+    def get_all(self) -> List[MovieSnapshot]:
+        return MovieSnapshot.query.all()
