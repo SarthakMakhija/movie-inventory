654d7635690a86aade2cfb742a78ad6211463cf1
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-12 13:11:30 +0530

    Abhijeet/Sarthak : Adds a test to assert total snapshots

--- flaskr/resources/movie_snapshots.py
+++ flaskr/resources/movie_snapshots.py
@@ -4,4 +4,7 @@ from flask_restful import Resource
 class MovieSnapshots(Resource):
 
     def get(self):
-        return []
+        return [
+            {
+            }
+        ]
--- tests/test_movie_snapshot.py
+++ tests/test_movie_snapshot.py
@@ -12,3 +12,8 @@ class MovieSnapshotTest(unittest.TestCase):
     def test_should_return_no_snapshots_given_no_snapshots_exist(self):
         response = TestClient.create().get("/movie-snapshots")
         self.assertEqual([], response.json)
+
+    def test_should_assert_total_snapshots_to_equal_1(self):
+        response = TestClient.create().get("/movie-snapshots")
+        movie_snapshots = response.json
+        self.assertEqual(len(movie_snapshots), 1)
