d3087be1a4564717ab71104adebebf9aae088344
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-13 00:29:39 +0530

    Abhijeet/Sarthak : Adds title and director column under movie_snapshot.py

--- flaskr/entity/movie_snapshot.py
+++ flaskr/entity/movie_snapshot.py
@@ -3,15 +3,9 @@ from flaskr.entity import db
 
 class MovieSnapshot(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500))
+    director = db.Column(db.String(250))
 
     def __init__(self, title: str, director: str):
-        self.__title = title
-        self.__director = director
-
-    @property
-    def title(self):
-        return self.__title
-
-    @property
-    def director(self):
-        return self.__director
+        self.title = title
+        self.director = director
