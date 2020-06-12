3f5ea32dfdb0078f063cdae2756423dba8ce2d9e
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-12 13:18:37 +0530

    Abhijeet/Sarthak : Refactors test_movie_snapshots.py

--- tests/test_movie_snapshots.py
+++ tests/test_movie_snapshots.py
@@ -3,17 +3,19 @@ import unittest
 from tests.fixtures.test_client import TestClient
 
 
-class MovieSnapshotTest(unittest.TestCase):
+class MovieSnapshotTests(unittest.TestCase):
+
+    __test_client = TestClient.create()
 
     def test_should_return_Ok_given_a_request_to_get_all_movie_snapshots(self):
-        response = TestClient.create().get("/movie-snapshots")
+        response = self.__test_client.get("/movie-snapshots")
         self.assertEqual(200, response.status_code)
 
     def test_should_return_no_snapshots_given_no_snapshots_exist(self):
-        response = TestClient.create().get("/movie-snapshots")
+        response = self.__test_client.get("/movie-snapshots")
         self.assertEqual([], response.json)
 
     def test_should_assert_total_snapshots_to_equal_1(self):
-        response = TestClient.create().get("/movie-snapshots")
+        response = self.__test_client.get("/movie-snapshots")
         movie_snapshots = response.json
         self.assertEqual(len(movie_snapshots), 1)
