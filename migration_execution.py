34c5a2b2af08cd2f13af5cde7204262ad0549757
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-13 01:39:46 +0530

    Abhijeet/Sarthak : Adds migration_execution.py and migration_execution_test.py for running migrations in test mode. Also adds flask-script as a dependency.

---
+++ migration_execution.py
@@ -0,0 +1,20 @@
+from typing import Type
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from flaskr.configuration import Configuration
+from flaskr.entity import db
+from flaskr.flask_application import Application
+
+
+class MigrationExecution:
+    def __init__(self, config: Type[Configuration]):
+        self.__config = config
+
+    def migration_manager(self) -> Manager:
+        app = Application(self.__config).flask_application
+        Migrate(app, db)
+        manager = Manager(app)
+        manager.add_command('db', MigrateCommand)
+        return manager
---
+++ tests/migration/migration_execution_test.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname("tests"), '.')))
+
+from migration_execution import MigrationExecution
+from tests.configuration.TestConfiguration import TestConfiguration
+
+
+class TestMigrationExecution(MigrationExecution):
+    def __init__(self):
+        super().__init__(TestConfiguration)
+
+
+if __name__ == '__main__':
+    TestMigrationExecution().migration_manager().run()
