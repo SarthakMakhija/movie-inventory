79b5c8907599621e291c1dd1114f163bc7df5490
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-13 17:13:49 +0530

    Abhijeet/Sarthak : Changes the application test to use TestConfiguration

--- tests/unit/test_flask_application.py
+++ tests/unit/test_flask_application.py
@@ -6,22 +6,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr.configuration import Configuration
 from flaskr.flask_application import Application
+from tests.configuration.configuration_test import TestConfiguration
 
 
 class ApplicationTest(unittest.TestCase):
 
     def test_should_return_an_instance_of_app_with_api_reference(self):
-        application = Application(Configuration)
+        application = Application(TestConfiguration)
         api = application.api
         self.assertTrue(type(api) == Api)
 
     def test_should_return_an_instance_of_app_with_db_reference(self):
-        application = Application(Configuration)
+        application = Application(TestConfiguration)
         app = application.db
         self.assertTrue(type(app) == SQLAlchemy)
 
     def test_should_return_an_instance_of_app_with_logger_reference(self):
-        application = Application(Configuration)
+        application = Application(TestConfiguration)
         app = application.logger
         self.assertTrue(type(app) == Logger)
 
