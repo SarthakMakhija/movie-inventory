4912a279bc0bf28e729accb9aed9954f5f41f897
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-13 17:17:07 +0530

    Abhijeet/Sarthak : Imports app in TestClient from __init__.py under integration test package

--- tests/fixtures/test_client.py
+++ tests/fixtures/test_client.py
@@ -1,8 +1,7 @@
-from flaskr.flask_application import Application
-from tests.configuration.configuration_test import TestConfiguration
+from tests.integration import app
 
 
 class TestClient:
     @staticmethod
     def create():
-        return Application(TestConfiguration).flask_application.test_client()
+        return app.test_client()
