0e6457cc8071ce01371571529427b3992bcff368
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-15 11:54:07 +0530

    Abhijeet/Sarthak : Adds behavior to ensure that a movie registration request without titles fails

--- flaskr/resource/movie_snapshots_registration_request_resource.py
+++ flaskr/resource/movie_snapshots_registration_request_resource.py
@@ -5,7 +5,7 @@ class MovieSnapshotsRegistrationRequestResource(Resource):
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument("titles", action="append")
+        parser.add_argument("titles", action="append", required=True)
         args = parser.parse_args()
 
         return "", 201
--- tests/unit/test_movie_snapshot_reqistration_request_resource.py
+++ tests/unit/test_movie_snapshot_reqistration_request_resource.py
@@ -13,3 +13,9 @@ class MovieSnapshotsRegistrationRequestResource(unittest.TestCase):
                                            content_type="application/json")
 
         self.assertEqual(201, response.status_code)
+
+    def test_should_return_Bad_Request_given_a_request_to_register_movie_snapshots_without_titles(self):
+
+        response = self.__test_client.post("/movie-snapshots/registration-request")
+
+        self.assertEqual(400, response.status_code)
