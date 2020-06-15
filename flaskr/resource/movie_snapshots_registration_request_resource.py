8323f5389ff0f184b15a4d827ceea661cb02ba06
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-15 11:52:16 +0530

    Abhijeet/Sarthak : Accepts titles in json payload for movie snapshot registration

--- flaskr/resource/movie_snapshots_registration_request_resource.py
+++ flaskr/resource/movie_snapshots_registration_request_resource.py
@@ -1,7 +1,11 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 class MovieSnapshotsRegistrationRequestResource(Resource):
 
     def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("titles", action="append")
+        args = parser.parse_args()
+
         return "", 201
--- tests/unit/test_movie_snapshot_reqistration_request_resource.py
+++ tests/unit/test_movie_snapshot_reqistration_request_resource.py
@@ -7,7 +7,9 @@ class MovieSnapshotsRegistrationRequestResource(unittest.TestCase):
     __test_client = TestClient.create()
 
     def test_should_return_Created_given_a_request_to_register_movie_snapshots(self):
-        response = self.__test_client.post("/movie-snapshots/registration-request")
+
+        response = self.__test_client.post("/movie-snapshots/registration-request",
+                                           data='{"titles": ["3 idiots"]}',
+                                           content_type="application/json")
+
         self.assertEqual(201, response.status_code)
-
-
