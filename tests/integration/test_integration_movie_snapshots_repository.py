81e46ac102bce5a26bdec773aaf1d0dfd0b72ed2
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-15 18:58:53 +0530

    Abhijeet/Sarthak : Adds an integration test for MovieSnapshotsRepository save_all method

---
+++ tests/integration/test_integration_movie_snapshots_repository.py
@@ -0,0 +1,18 @@
+import unittest
+
+from flaskr.repository.movie_snapshots_repository import MovieSnapshotsRepository
+from tests.fixtures.movie_snapshots_builder import MovieSnapshotsBuilder
+from tests.fixtures.movie_snapshots_fixture import MovieSnapshotsFixture
+from tests.fixtures.test_client import TestClient
+
+
+class MovieSnapshotResourceIntegrationTest(unittest.TestCase):
+    __client = TestClient.create()
+
+    def test_should_save_all_movie_snapshots(self):
+        movie_snapshot = MovieSnapshotsBuilder.any_snapshot().finish()
+        snapshot_ids = MovieSnapshotsRepository().save_all([movie_snapshot])
+        self.assertIsNotNone(snapshot_ids[0])
+
+    def tearDown(self) -> None:
+        MovieSnapshotsFixture.delete_all()
