82a627d8caa3a5b73ff8d89840041983dbfdfa93
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-15 12:28:11 +0530

    Abhijeet/Sarthak : Adds logging

--- flaskr/model/movie_snapshot_registration_request.py
+++ flaskr/model/movie_snapshot_registration_request.py
@@ -4,3 +4,7 @@ from typing import List
 class MovieSnapshotsRegistrationRequest:
     def __init__(self, titles: List[str]):
         self.__titles = titles[:]
+
+    @property
+    def titles(self):
+        return self.__titles
--- flaskr/resource/movie_snapshots_registration_request_resource.py
+++ flaskr/resource/movie_snapshots_registration_request_resource.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 
+from flaskr.logger_factory import LoggerFactory
 from flaskr.model.movie_snapshot_registration_request import MovieSnapshotsRegistrationRequest
 
 
@@ -15,6 +16,10 @@ def parse_movie_snapshots_registration_request(func):
 
 class MovieSnapshotsRegistrationRequestResource(Resource):
 
+    def __init__(self):
+        self.logger = LoggerFactory.instance().logger()
+
     @parse_movie_snapshots_registration_request
     def post(self, movie_snapshots_registration_request: MovieSnapshotsRegistrationRequest):
+        self.logger.info(f"Received a request for registering movie snapshots with titles = {movie_snapshots_registration_request.titles}")
         return "", 201
