88b9daafbb116af2c4d9fffa9c273e5c1687289c
sarthakmakhija <sarthak.makhija@gmail.com>
2020-06-16 15:12:17 +0530

    Abhijeet/Sarthak : Refactors movie_snapshots_registration_service.py and invokes get_movies_response_for_NEW of omdb_movie_client

--- flaskr/model/response.py
+++ flaskr/model/response.py
@@ -27,11 +27,12 @@ class Failure(Generic[E]):
 
 
 class Response(Generic[T, E]):
+
     def __init__(self):
         self.__success: List[Success[T]] = []
         self.__failure: List[Failure[E]] = []
 
-    def add(self, response: Union[Success, Failure]):
+    def add(self, response: Union[Success[T], Failure[E]]):
         if isinstance(response, Success):
             self.__add_success(response)
         else:
@@ -40,12 +41,17 @@ class Response(Generic[T, E]):
     def success_at(self, index) -> Success[T]:
         return self.__success[index]
 
+    def success_count(self) -> int:
+        return len(self.__success)
+
     def failure_count(self) -> int:
         return len(self.__failure)
+
+    def all_success_t(self) -> List[T]:
+        return [success.t() for success in self.__success]
 
     def __add_success(self, t: T):
         self.__success.append(t)
 
     def __add_failure(self, e: E):
         self.__failure.append(e)
-
--- flaskr/service/movie_snapshots_registration_service.py
+++ flaskr/service/movie_snapshots_registration_service.py
@@ -3,7 +3,8 @@ from typing import List, Dict
 from flaskr.entity.movie_snapshot import MovieSnapshot
 from flaskr.logger_factory import LoggerFactory
 from flaskr.model.movie_snapshot_registration_request import MovieSnapshotsRegistrationRequest
-from flaskr.omdb_movie_client import OmdbMovieClient
+from flaskr.model.response import Response
+from flaskr.omdb_movie_client import OmdbMovieClient, Movie
 from flaskr.repository.movie_snapshots_repository import MovieSnapshotsRepository
 
 
@@ -15,12 +16,20 @@ class MovieSnapshotsRegistrationService:
         self.logger = LoggerFactory.instance().logger()
 
     def register_snapshots_for(self, a_request: MovieSnapshotsRegistrationRequest) -> List[str]:
-        movies = self.omdb_client.get_movies_for(a_request.titles)
-        self.logger.info(f"Received a total of {len(movies)} movies for {len(a_request.titles)} titles")
+        movie_response = self.__get_movie_response_for(a_request.titles)
+        movies: List[Movie] = movie_response.all_success_t()
         snapshots: List[MovieSnapshot] = [movie.to_movie_snapshot() for movie in movies]
 
         if snapshots:
             return self.movie_snapshots_repository.save_all(snapshots)
         else:
             return []
+
+    def __get_movie_response_for(self, titles):
+        movie_response: Response[Movie, str] = self.omdb_client.get_movies_response_for_NEW(titles)
+        self.logger.info(f"Successfully got a count of {movie_response.success_count()} movies with a "
+                         f"failure count of {movie_response.failure_count()}, for a total of "
+                         f"{len(titles)} titles")
+
+        return movie_response
 
